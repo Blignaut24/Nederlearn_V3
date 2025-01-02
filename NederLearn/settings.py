17207654d13799fcd88c923dbc4b8918e28dc258
Johann Jurgens Blignaut <112351511+Blignaut24@users.noreply.github.com>
2025-01-03 00:09:56 +0100

    chore: add environment variable and secret key configuration setup

--- NederLearn/settings.py
+++ NederLearn/settings.py
@@ -10,17 +10,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+if os.path.exists('env.py'):
+    import env
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-kwfcm=s-^&5ep)!$j$g*un^c#09ubq1x-a)jk%6uhmt6r%_x^0"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
